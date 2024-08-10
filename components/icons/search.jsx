50b36c73ef87b5ca80c073d7dae22e32cf29593e
rsjzoe <rsjzoe@gmail.com>
2024-08-10 16:35:57 +0300

    update icon search

--- components/icons/search.jsx
+++ components/icons/search.jsx
@@ -1,33 +1,30 @@
 import * as React from "react"
-import Svg, { G, Path, Circle, Defs, ClipPath } from "react-native-svg"
+import Svg, { Path } from "react-native-svg"
 const Search = (props) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
+    width={26}
+    height={26}
     fill="none"
-    viewBox="0 0 24 24"
-    color={"#000"}
+    viewBox="0 -0.5 25 25"
+    color={'#000'}
     {...props}
   >
-    <G clipPath="url(#a)">
-      <Circle
-        cx={10.5}
-        cy={10.5}
-        r={6.5}
-        stroke="currentColor"
-        strokeLinejoin="round"
-      />
-      <Path
-        fill="currentColor"
-        d="M19.646 20.354a.5.5 0 0 0 .708-.708l-.708.708Zm.708-.708-5-5-.708.708 5 5 .708-.708Z"
-      />
-    </G>
-    <Defs>
-      <ClipPath id="a">
-        <Path fill="currentColor" d="M0 0h24v24H0z" />
-      </ClipPath>
-    </Defs>
+    <Path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M5.5 10.765a5.765 5.765 0 1 1 11.53 0 5.765 5.765 0 0 1-11.53 0Z"
+      clipRule="evenodd"
+    />
+    <Path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M17.029 16.53 19.5 19"
+    />
   </Svg>
 )
 export default Search
